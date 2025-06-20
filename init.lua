7004a154c4ad01df8822eb7c437f68973c548e19
Akdev Saha <akdevsaha2003@gmail.com>
2025-06-21 00:49:33 +0530

    nvim update 1.0

--- init.lua
+++ init.lua
@@ -1,49 +1,19 @@
-require 'core.options'
-require 'core.keymaps'
+    require 'core.options'
+    require 'core.keymaps'
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
+    local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+      local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+      local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+      if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+      end
+    end
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+    vim.opt.rtp:prepend(lazypath)
 
---
--- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-{
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- fill any relevant options here
-  },
-},
-{
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      -- ...
+    require('lazy').setup({
+    require 'plugins.neotree',
+    require 'plugins.colortheme',
+    require 'plugins.bufferline'
     })
-
-    vim.cmd('colorscheme github_dark_default')
-  end,
-}    
-})
